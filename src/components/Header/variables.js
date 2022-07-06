de6a5073ccf4fab87f111f176ef3f72f92c4f433
jeff122iq <jeff122iq@gmail.com>
2022-07-06 14:11:34 +0300

    fourth fix

--- src/components/Header/variables.js
+++ src/components/Header/variables.js
@@ -7,7 +7,7 @@ export const headerNavData = [
     {
         id: 1,
         name: 'Get directions',
-        link: '',
+        link: 'https://www.google.com/maps/place/Storm+in+a+Coffeepot/@48.1210957,11.5607772,17z/data=!3m1!4b1!4m5!3m4!1s0x479e75eb67b769a7:0x75bed7038a1fa3ca!8m2!3d48.1211048!4d11.560745',
         icon: <i class="fa-solid fa-suitcase"></i>
     },
 ]
