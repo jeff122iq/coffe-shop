febe2e1391da08b73413a55f0f933fe54d5c3559
jeff122iq <jeff122iq@gmail.com>
2022-07-06 13:48:41 +0300

    second fix

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,7 @@ import './index.css'
 
 export default function NavBar({ open, setOpen }) {
   return (
-    <div className={`navBar ${open ? 'close' : ''}`}>
+    <div className={`${open === true ? 'navBar open' : 'navBar'}`}>
         <a href='#contact' onClick={() => setOpen(!open)}>Contact</a>
     </div>
   )
